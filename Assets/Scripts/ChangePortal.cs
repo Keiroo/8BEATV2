3d72828df385d618efe4b762b6c2c2bcf49f9b26
Keiroo <orynia55@o2.pl>
2018-07-18 14:13:34 +0200

    Add portal color logic

---
+++ Assets/Scripts/ChangePortal.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangePortal : MonoBehaviour {
+
+    public Sprite[] sprites;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Change(Button button)
+    {
+        if (button.name == "RedButton")
+        {
+            GetComponentInChildren<SpriteRenderer>().sprite = sprites[0];
+        }
+        else if (button.name == "GreenButton")
+        {
+            GetComponentInChildren<SpriteRenderer>().sprite = sprites[1];
+        }
+        else if (button.name == "BlueButton")
+        {
+            GetComponentInChildren<SpriteRenderer>().sprite = sprites[2];
+        }
+    }
+}
