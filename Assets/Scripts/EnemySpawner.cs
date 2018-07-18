9a021d5229a1b9d10fa6747a1d45292296d7932b
Keiroo <orynia55@o2.pl>
2018-07-18 18:40:23 +0200

    Set enemy spawn

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+
+    public GameObject[] Enemies;
+
+    float minSpawnTime = 0.5f;
+    float maxSpawnTime = 2.0f;
+
+	// Use this for initialization
+	void Start ()
+    {
+        StartCoroutine(SpawnCoroutine());
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        
+	}
+
+    IEnumerator SpawnCoroutine()
+    {
+        while (true)
+        {
+            float spawnTime = Random.Range(minSpawnTime, maxSpawnTime);
+            yield return new WaitForSeconds(spawnTime);
+            print(spawnTime);
+            SpawnEnemy();
+        }        
+    }
+
+    public void SpawnEnemy()
+    {
+        int enemyID = Random.Range(0, 3);
+        Instantiate(Enemies[enemyID], transform);
+        print(transform.position);
+
+    }
+}
