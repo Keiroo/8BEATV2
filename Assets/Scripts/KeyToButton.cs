8e964858d0676de60fb8d18949f60d761870155a
Keiroo <orynia55@o2.pl>
2018-07-23 20:35:25 +0200

    Add keyboard control

---
+++ Assets/Scripts/KeyToButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeyToButton : MonoBehaviour {
+
+    public KeyCode keyCode;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(keyCode))
+        {
+            GetComponent<Button>().onClick.Invoke();
+        }
+	}
+}
