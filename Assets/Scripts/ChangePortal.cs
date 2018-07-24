4dad01b90003985159f3d706e0a92e9d8d12ff6e
Keiroo <orynia55@o2.pl>
2018-07-24 23:05:48 +0200

    Add spectrum visualiser, menu background and new heart sprite

--- Assets/Scripts/ChangePortal.cs
+++ Assets/Scripts/ChangePortal.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class ChangePortal : MonoBehaviour {
 
     public Sprite[] sprites;
+    public GameObject[] spectrums;
 
 	// Use this for initialization
 	void Start () {
@@ -22,14 +23,26 @@ public class ChangePortal : MonoBehaviour {
         if (button.name == "RedButton")
         {
             GetComponentInChildren<SpriteRenderer>().sprite = sprites[0];
+            ChangeSpectrumsColor(new Color(0.2f, 0f, 0f));
         }
         else if (button.name == "GreenButton")
         {
             GetComponentInChildren<SpriteRenderer>().sprite = sprites[1];
+            ChangeSpectrumsColor(new Color(0f, 0.2f, 0f));
         }
         else if (button.name == "BlueButton")
         {
             GetComponentInChildren<SpriteRenderer>().sprite = sprites[2];
+            ChangeSpectrumsColor(new Color(0f, 0f, 0.2f));
+        }
+    }
+
+    private void ChangeSpectrumsColor(Color color)
+    {
+        foreach (GameObject spec in spectrums)
+        {
+            spec.GetComponent<SimpleSpectrum>().colorMin = color;
+            spec.GetComponent<SimpleSpectrum>().RebuildSpectrum();
         }
     }
 }
