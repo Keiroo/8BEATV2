a1e2b2e2db984936cc11195577df746833f21009
Keiroo <orynia55@o2.pl>
2018-07-25 16:07:06 +0200

    Fix portal bug
    Portal was moving up, no goes back to previous position

--- Assets/Scripts/PortalAnimation.cs
+++ Assets/Scripts/PortalAnimation.cs
@@ -45,6 +45,7 @@ public class PortalAnimation : MonoBehaviour {
             }
             else
             {
+                transform.localPosition = orgPosition;
                 enabled = false;
             }
         }
