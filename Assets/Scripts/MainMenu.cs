6eae5cb8893352e2b84b92294b22662f99a0bbd3
Keiroo <orynia55@o2.pl>
2018-09-15 18:08:29 +0200

    Add main menu BGM

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,19 +6,36 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour {
 
     public GameObject FadeImage;
+    public AudioSource AudioSource;
+    public AudioClip BGM;
+    public AudioClip Button;
 
 	// Use this for initialization
 	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+		if (AudioSource != null)
+        {
+            if (BGM != null)
+            {
+                AudioSource.clip = BGM;
+                AudioSource.loop = true;
+                AudioSource.Play();
+            }
+        }
 	}
 
     public void LoadGameScene()
     {
+        if (AudioSource != null)
+        {
+            if (Button != null)
+            {
+                AudioSource.Stop();
+                AudioSource.loop = false;
+                AudioSource.volume = 0.5f;
+                AudioSource.clip = Button;
+                AudioSource.Play();
+            }
+        }
         FadeImage.GetComponent<Fade>().enabled = true;
     }
 }
